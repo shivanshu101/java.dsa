14d1f15eb5cf2b5578d98848080a2591603ec25e
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-09-22 14:29:55 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,20 @@
+public class loop{
+    
+    public static void main(String[] args) {
+        
+   // for(int i = 0; i < 11; i++){
+     //   System.out.println(i+" ");
+    //}
+   // int i = 0;
+    //while(i < 11){
+       // System.out.println(i);
+        //i = i + 1;
+    //}
+    int i = 0;
+    do{
+       System.out.println(i);
+       i = i + 1;
+    } while(i < 11);
+    
+    }
+}
