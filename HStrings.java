f32daaa6a5c2f8022ca874120944196eee6a85ff
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-12 03:53:10 +0530

    Add files via upload

---
+++ HStrings.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class HStrings{
+    public static void main(String[] args){
+
+      //  Scanner sc = new Scanner(System.in);   // apna collage
+     //   int size = sc.nextInt();
+     //   String[] array = new String[size];
+     //   int totLength = 0;
+
+    //    for(int i=0; i<size; i++){
+    //        array[i] = sc.next();
+    //        totLength = array[i].length();
+  
+    //    }   
+         
+     //   System.out.println(totLength);
+
+        // problem 1        codewithharry
+    //String name = "tony stark";
+   // name = name.toLowerCase();
+   // System.out.println(name);    
+
+    // problem 2
+   // String text = "to my     friend";
+   // text = text.replace(" ", "_");
+   // System.out.println(text);
+
+    // problem 3
+   // String letter = "Dear <|name|>, thanks a lot!";
+   // letter = letter.replace("<|name|>", "Harry");
+   // System.out.println(letter);
+
+    //problem 4
+    String mystring = "this String contains double   and triple  spaces";
+   System.out.println(mystring.indexOf("  "));  
+   System.out.println(mystring.indexOf("    "));     
+
+   // problem 5
+   //  String myletter = "Dear Harry,\n\tThis Java Course is Nice.\nThanks!";   
+    // System.out.println(myletter);  
+    }
+}
