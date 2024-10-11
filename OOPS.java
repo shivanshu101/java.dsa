bfa95e63e8c50d9354459f96634066ff64b75834
Shivanshu Kumar <shivanshukumar4929@gmail.com>
2024-10-12 03:48:55 +0530

    Add files via upload

--- OOPS.java
+++ OOPS.java
@@ -1,39 +1,22 @@
-abstract class Animal {
-    abstract void walk();
-    void breathe() {
-        System.out.println("This animal breathes air");
-    }
-    Animal() {
-        System.out.println("You are about to create an Animal.");
-    }
+class Student {
+    static String school;
+    String name;   
  }
  
  
- class Horse extends Animal {
-    Horse() {
-        System.out.println("Wow, you have created a Horse!");
-    }
-    void walk() {
-        System.out.println("Horse walks on 4 legs");
-    }
- }
- 
- 
- class Chicken extends Animal {
-    Chicken() {
-        System.out.println("Wow, you have created a Chicken!");
-    }
-    void walk() {
-        System.out.println("Chicken walks on 2 legs");
-    }
- }
- 
- 
- public class OOPS{
+ public class OOPS {
     public static void main(String args[]) {
-       Horse horse = new Horse();
-       horse.walk();
-       horse.breathe();
+        Student.school = "JMV";
+        Student s1 = new Student();
+        Student s2 = new Student();
+ 
+ 
+        s1.name = "Meena";
+        s2.name = "Beena";
+ 
+ 
+        System.out.println(s1.school);
+        System.out.println(s2.school);
     }
  }
  
